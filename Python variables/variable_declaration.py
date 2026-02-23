d40fbc822cbb8a2ad813d683f80a994f28bb7573
Dev Raghuwanshi <devraghuwanshi18@gmail.com>
2026-02-24 00:47:47 +0530

    Add Python variables
    This repository covers Python variable concepts such as declaration, naming conventions, data types, and practical examples for better understanding.

---
+++ Python variables/variable_declaration.py
@@ -0,0 +1,26 @@
+
+#variable are containers that stores data values.
+x = 10
+y = "Dev"
+#variable do not need to be defined with any particular type , and can even change type after they have been set
+print(x)
+print(y)
+
+
+#Assigning multiple values:
+a,b,c = "orange" , "blue" , "green"
+print(a , b , c)
+e = f= g ="10"
+print(e , f , g)
+
+#Output variable:
+outPut = "This is example of output variable."
+print(outPut)
+
+l = "this"
+m = "is"
+n = "example"
+o = "of"
+p = "output"
+r = "variable."
+print(l,m,n,o,p,r)
