45d560fa95036203da68463b4dfe5046c00da6d0
Dev Raghuwanshi <devraghuwanshi18@gmail.com>
2026-03-05 01:05:44 +0530

    Add Python String
    This file contains basic examples of string handling in Python, including string creation, common string methods, and simple manipulation techniques used for learning and practice.

---
+++ Python Strings/String_declaration.py
@@ -0,0 +1,18 @@
+# Assigning string to variable  a.
+a = "This is an example of String"
+print(a)
+
+# Multiline String
+ma = """This 
+is 
+multiline string"""
+print(ma)
+
+# You can find length of string using len(str_name) function.
+string = "This string is made to check length"
+print(len(string))
+
+# Check String - in keyword - ro check if certain phrase is present in a string 
+text = "Check if this string includes free"
+print("free" in text)
+
