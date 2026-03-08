178b57d9f74175bcac1cc2d0acc697a3449abcf2
Dev Raghuwanshi <devraghuwanshi18@gmail.com>
2026-03-08 14:26:09 +0530

    Add count() , find() , dir() and help() methods

---
+++ Python Strings/String methods/count() and find().py
@@ -0,0 +1,8 @@
+str = "this is example of count and find this"
+# count('value') - counts the number of times a value occurs in string.
+print(str.count('i'))
+print(str.count('this'))
+
+# find('value') - finds the starting index of value. 
+print(str.find('example'))
+print(str.find('i'))
---
+++ Python Strings/String methods/dir() and help().py
@@ -0,0 +1,9 @@
+str1 = 'this is an example of dir() and help()'
+
+
+# dir(strName) - function to print out all methods and attributes of string.
+print(dir(str1))
+
+# help(strName) - prints discription of all methods and attributes of string.
+print(help(str))
+print(help(str.lower))
