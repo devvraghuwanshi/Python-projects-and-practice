17d2751c0642b95f6660a2aa6daa6ed94bcddfe2
Dev Raghuwanshi <devraghuwanshi18@gmail.com>
2026-04-13 00:21:50 +0530

    Add Python Build-in functions
    A curated collection of Python built-in functions with examples and explanations.

---
+++ Python Build-In functions/range.py
@@ -0,0 +1,17 @@
+# range() is a built-in function used to generate a sequence of numbers.
+
+# SYNTAX - range(start,stop,step)
+
+nums = range(10)  #0-9
+print(list(nums))
+print(type(nums))
+
+
+nums = range(1,11) #1-10
+print(list(nums))
+
+nums = range(1,11,3)
+print(list(nums))
+
+nums = range(10,0,-1)
+print(list(nums))
---
+++ Python Build-In functions/user_input.py
@@ -0,0 +1,6 @@
+# input() - Built in funtion used to take input from users (input is taken in string)
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+sum = num1 + num2
+print("Sum is : " , sum)
