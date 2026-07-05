3ddfdc4ee43cab31ecdc7ef63497582cc577da3d
Dev Raghuwanshi <devraghuwanshi18@gmail.com>
2026-07-05 22:59:54 +0530

    Add parameter v/s argument

---
+++ Python Functions/Parameter_vs_Argument.py
@@ -0,0 +1,8 @@
+# Parameter - variables listed inside parentheses in the function defination
+# Argument - actual value that is sent to the function when it is called
+
+def greet(name):               #name is parameter
+    print(f"Hello {name}")
+
+
+greet("Dev")                   #Dev is argument
